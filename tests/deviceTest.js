8b3d02b46c2a829e70cd842d8c44d1df1b77a8e9
ChrisCanvas <christopher@canvas.com>
2025-01-21 18:41:51 -0500

    New

--- tests/deviceTest.js
+++ tests/deviceTest.js
@@ -1,82 +1,195 @@
-// // This is used to do expectations(similart to assert) and test to start your test cases. 
-// const {test, expect} = require("@playwright/test")
+import { expect, Locator, Page } from '@playwright/test';
+import { Common } from './Common';
+import { CreateTag } from './CreateTag';
+import { getCellsByColumnName } from '../utils/DataGrid/getCellsByColumnName';
 
-// // Navigates to the url before each test
-// test.beforeEach(async ({ page }) => {
-//     // Go to the starting url before each test.
-//     await page.goto('/');
-// });
-// test("first test", async ({page})=>{
-//     await page.goto("https://the-internet.herokuapp.com/");
-//     await page.locator('//a[@href="/abtest"]').click();
-//     // page.url captures the current page url
-//     const url = page.url();
-//     expect(url).toBe('https://the-internet.herokuapp.com/abtest')
-// })
+export class Device {
+  page: Page;
+  deviceNavigationButton: Locator;
+  searchFieldText: Locator;
+  firstTagLine: Locator;
+  addTagButton: Locator;
+  ztm_gateway_field: Locator;
+  addRouteButton: Locator;
+  deleteZTMGatewayButton: Locator;
+  revokeReleaseButton: Locator;
+  revokeAuthKeyButton: Locator;
+  backButton: Locator;
+  deleteDeviceButton: Locator;
+  deleteConfirmButton: Locator;
 
-// test("enter text", async ({page})=>{
-//     await page.goto("https://the-internet.herokuapp.com/login");
-//     const userName = page.locator("#username");
-//     // need fine-grained keyboard events, you use page.type.
-//     await userName.type('tomsmith', {delay:1000});
-//     const password = page.locator("#password")
-//     // page.fill to be used whenever one needs to fill a form
-//     await password.fill('SuperSecretPassword!');
-//     const login_button = page.locator('//button[@class="radius"]')
-//     await login_button.click();
+  //Element selectors defined
+  constructor(thePage: Page) {
+    this.page = thePage;
 
-// })
+    //Text field
+    this.searchFieldText = this.page.locator('[data-e2e="DataGrid-Searchbar"]');
+    this.ztm_gateway_field = this.page.locator('[aria-label="ZTM Gateway route 1"]');
 
-// test("enter checkbox", async ({page})=>{
-//     await page.goto("https://the-internet.herokuapp.com/checkboxes");
-//     const checkbox2 = page.locator('#checkboxes>input:nth-child(1)')
-//     await checkbox2.check();
-//     // Verify that the checkbox is checked
-//     expect(await checkbox2.isChecked()).toBeTruthy()
+    //Button
+    this.deviceNavigationButton = this.page.locator('[data-e2e="devices-navigation-link"]');
+    this.addTagButton = this.page.locator('[data-e2e="viewMachine-addTagsButton"]');
+    this.addRouteButton = this.page.locator('[data-e2e="viewMachine-ZTM-Gateway-Add-Route-button"]');
+    this.deleteZTMGatewayButton = this.page.locator('[data-e2e="viewMachine-delete-ZTM-gateway-button-1"]');
+    this.revokeReleaseButton = this.page.locator('[data-e2e="revoke-auth-key"]');
+    this.revokeAuthKeyButton = this.page.locator('[data-e2e="revoke-device-auth-key"]');
+    this.backButton = this.page.locator('[aria-label="go back to devices"]');
+    this.deleteDeviceButton = this.page.locator('[data-e2e="viewMachine-delete-button"]');
+    this.deleteConfirmButton = this.page.locator('[data-e2e="delete-device"]');
+    //List
+    this.firstTagLine = this.page.locator('[data-field="name"]').nth(1);
+  }
 
-// })
+  readonly slowExpect = expect.configure({ timeout: 20000 });
 
-// test("select dropdown", async ({page})=>{
-//     await page.goto("https://the-internet.herokuapp.com/dropdown");
-//     const dropdown = page.locator("select#dropdown")
-//     await dropdown.selectOption('Option 1')
-//     // verify the selected dropdown is available
-//     const value = await dropdown.textContent()
-//     await expect(value.includes("Option 1")).toBeTruthy
+  //Navigation functions
+  visitDeviceManagement = async () => {
+    await this.deviceNavigationButton.click();
+    await expect(this.page).toHaveURL(/devices/);
+    await expect(this.page.locator('[data-e2e="machines-pageHeader"]')).toBeVisible;
+  };
 
-// })
+  searchForDevice = async (deviceName: string) => {
+    // Navigate to policies new page
+    await this.page.goto('/' + 'devices');
+    await this.page.waitForTimeout(3500);
+    await this.page.reload();
+    // Reload twice due to jenkins, taking some time to find device
+    await this.page.reload();
+    await this.page.locator('[data-field="name"]').nth(1).waitFor();
+    await this.searchFieldText.fill(deviceName);
+    await this.page.locator('[data-field="name"]').nth(1).waitFor();
+    await expect(this.page.locator('[data-field="name"]').nth(1)).toContainText(deviceName);
+    await this.page.waitForTimeout(1000);
+  };
 
-// // Verify if an item on the page is visible or not
+  verifyDeviceDetailPage = async (deviceName: string) => {
+    await this.visitDeviceManagement();
+    await this.searchForDevice(deviceName);
+    await this.page.waitForTimeout(3000);
+    await this.page.getByText(deviceName).click();
+    await this.slowExpect(this.page.locator('[data-e2e="viewMachine-details-card"]')).toBeVisible();
 
+    await expect(this.page.locator('h6.MuiTypography-root.MuiTypography-h6')).toHaveText(deviceName);
+    await expect(this.page.locator('[id="details-headingDetails"]')).toBeVisible;
 
-// // Section on assertions
-// test("expect assertions", async({page})=>{
-//     await page.goto("https://the-internet.herokuapp.com/");
-//     const ab = page.locator('//a[@href="/abtest"]')
-//     // expect the text of the identifier
-//     const text  = await ab.textContent();
-//     expect(text).toBe('A/B Testing')
-//     // or
-//     expect(text).toEqual('A/B Testing')
-// })
+    const common = new Common(this.page);
+    await common.editButton.waitFor({ state: 'visible' });
 
-// test("expect element has text", async({page})=>{
-//     await page.goto("https://the-internet.herokuapp.com/");
-//     const ab = page.locator('//a[@href="/abtest"]');
-//     // verify the actual text is present
-//     await expect(ab).toHaveText('A/B Testing');
-// })
+    await expect(this.page.getByText('Hostname')).toBeVisible();
+    await expect(this.page.getByText('OS', { exact: true })).toBeVisible();
+    await expect(this.page.getByText('OS Version')).toBeVisible();
+    await expect(this.page.getByText('Location').first()).toBeVisible();
+    await expect(this.page.getByText('IPv4')).toBeVisible();
+    await expect(this.page.getByText('IPv6')).toBeVisible();
 
-// test("expect element to be visible", async({page})=>{
-//     //await page.goto("https://the-internet.herokuapp.com/");
-//     const ab = page.locator('//a[@href="/abtest"]');
-//     // verify the actual text is present
-//     await expect(ab).toBeVisible();
-// })
+    await expect(this.page.getByText('Registered domain name')).toBeVisible();
+    await expect(this.page.getByText('Associated user')).toBeVisible();
+    await expect(this.page.getByText('Connectivity')).toBeVisible();
 
-// test("expect page to have title", async({page})=>{
-//     //await page.goto("https://the-internet.herokuapp.com/");
-//     // verify the page title is present
-//     await expect(page).toHaveURL('https://the-internet.herokuapp.com/')
-// })
+    await expect(this.page.getByRole('heading', { name: 'Auth key registration' })).toBeVisible();
+    await expect(this.page.getByRole('heading', { name: 'ZTM Internet Gateway' })).toBeVisible();
+  };
 
+  addTagToDevice = async (deviceName: string, tagName: string) => {
+    await this.page.goto('/' + 'devices');
+    await this.visitDeviceManagement();
+    await this.searchForDevice(deviceName);
+    await this.page.waitForTimeout(3000);
+    await this.page.getByText(deviceName).click();
+    await this.slowExpect(this.page.locator('[data-e2e="viewMachine-details-card"]')).toBeVisible();
+    const common = new Common(this.page);
+    await common.editButton.click();
+    await this.addTagButton.click();
+    const create_tag = new CreateTag(this.page);
+    await create_tag.searchForTagPolicies(tagName);
+    await this.page.locator('[data-field="description"]').nth(1).click();
+    await common.saveButton.click();
+  };
+
+  addRoute = async (deviceName: string, route: string) => {
+    await this.page.goto('/' + 'devices');
+    const devicePage = new Device(this.page);
+    await devicePage.searchForDevice(deviceName);
+    await this.page.waitForTimeout(3000);
+    await this.page.getByText(deviceName).click();
+    await this.slowExpect(this.page.locator('[data-e2e="viewMachine-details-card"]')).toBeVisible();
+    const common = new Common(this.page);
+    await common.editButton.click();
+    await devicePage.addRouteButton.click();
+    await devicePage.ztm_gateway_field.fill(route);
+    await common.saveButton.click();
+
+    await expect(this.page.locator('[data-e2e="viewMachine-update-successful-success"]')).toBeVisible();
+    //Needs better identifier
+    await expect(this.page.locator('.css-1kxrhf3').nth(0)).toHaveText(route);
+  };
+
+  deleteRoute = async (deviceName: string) => {
+    const devicePage = new Device(this.page);
+    await devicePage.searchForDevice(deviceName);
+    await this.page.waitForTimeout(3000);
+    await this.page.getByText(deviceName).click();
+    await this.slowExpect(this.page.locator('[data-e2e="viewMachine-details-card"]')).toBeVisible();
+    const common = new Common(this.page);
+    await common.editButton.click();
+    await devicePage.deleteZTMGatewayButton.click();
+    await common.saveButton.click();
+
+    await expect(this.page.locator('[data-e2e="viewMachine-update-successful-success"]')).toBeVisible();
+  };
+
+  revokeRelease = async (deviceName: string) => {
+    await this.page.goto('/' + 'devices');
+    const devicePage = new Device(this.page);
+    await devicePage.searchForDevice(deviceName);
+    await this.page.waitForTimeout(3000);
+    await this.page.getByText(deviceName).click();
+    await this.slowExpect(this.page.locator('[data-e2e="viewMachine-details-card"]')).toBeVisible();
+    const common = new Common(this.page);
+    await common.editButton.click();
+    await devicePage.revokeReleaseButton.click();
+    await devicePage.revokeAuthKeyButton.click();
+    await this.page.waitForTimeout(500);
+    await common.editButton.click();
+
+    await expect(devicePage.revokeReleaseButton).not.toBeVisible();
+  };
+
+  enableDisableInternetGateway = async (deviceName: string) => {
+    await this.page.goto('/' + 'devices');
+    const devicePage = new Device(this.page);
+    await devicePage.searchForDevice(deviceName);
+    await this.page.waitForTimeout(3000);
+    await this.page.getByText(deviceName).click();
+    await this.slowExpect(this.page.locator('[data-e2e="viewMachine-details-card"]')).toBeVisible();
+    const common = new Common(this.page);
+
+    await common.editButton.click();
+    await this.slowExpect(this.page.locator('[data-e2e="viewMachine-ZTM-Gateway-Add-Route-button"]')).toBeVisible();
+
+    if (await this.page.locator('[data-e2e="viewMachine-ZTM-Internet-Gateway-Switch-Off"]').isVisible()) {
+      await this.page.locator('[data-e2e="viewMachine-ZTM-Internet-Gateway-Switch-Off"]').check();
+      await common.saveButton.click();
+
+      await expect(this.page.locator('[data-e2e="viewMachine-update-successful-success"]')).toBeVisible();
+      await expect(this.page.getByText('Enabled')).toBeVisible();
+    } else {
+      await this.page.locator('[data-e2e="viewMachine-ZTM-Internet-Gateway-Switch-On"]').isVisible();
+      await this.page.locator('[data-e2e="viewMachine-ZTM-Internet-Gateway-Switch-On"]').click();
+      await common.saveButton.click();
+
+      await expect(this.page.locator('[data-e2e="viewMachine-update-successful-success"]')).toBeVisible();
+      await expect(this.page.getByText('Disabled')).toBeVisible();
+    }
+  };
+
+  deleteDevice = async (deviceName: string) => {
+    await this.searchForDevice(deviceName);
+    await this.page.waitForTimeout(3000);
+    await this.page.getByText(deviceName).click();
+    await this.deleteDeviceButton.click();
+    await this.deleteConfirmButton.click();
+    await expect(this.page.locator('[data-e2e="delete-machine-success"]')).toBeVisible();
+  };
+}
